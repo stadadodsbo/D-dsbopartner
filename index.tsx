18e6db2e4462264024429301ce96eeb5cded5cc2
stadadodsbo <stadadodsbo@gmail.com>
2026-01-12 21:26:43 +0700

    feat: Upgrade Angular and RxJS versions
    Updates Angular core packages and RxJS to their latest stable versions (18.2.0 and 7.8.0 respectively). This includes updating dependencies in `package.json` and the import map in `index.html`. Also removes zoneless change detection as it's now the default behavior.

--- index.tsx
+++ index.tsx
@@ -1,13 +1,12 @@
+import 'zone.js';
 import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './src/app.component';
-import { provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation, withInMemoryScrolling } from '@angular/router';
 import { routes } from './src/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZonelessChangeDetection(),
     provideRouter(
       routes, 
       withHashLocation(),
